8d7eee7c8147dedabdbb1c25872bd0b876b4e5fb
verma-kartik <kartikv.vero058@gmail.com>
2023-02-09 00:04:42 +0530

    Testing & Cicrle CI

--- Task-2/main_test.go
+++ Task-2/main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/assert/v2"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -15,7 +17,7 @@ func SetUpRouter() *gin.Engine {
 }
 
 func TestHomepageHandler(t *testing.T) {
-	mockResponse := `{"message":"Welcome CRUD API with Golang"}`
+	mockResponse := `{"message":"Welcome to CRUD API with Golang"}`
 	r := SetUpRouter()
 	r.GET("/", HomepageHandler)
 	req, _ := http.NewRequest("GET", "/", nil)
@@ -27,6 +29,37 @@ func TestHomepageHandler(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
-func main() {
+func TestGetCompaniesHandler(t *testing.T) {
+	r := SetUpRouter()
+	r.GET("/books", GetBooksHandler)
+	req, _ := http.NewRequest("GET", "/books", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
 
+	var books []Book
+	json.Unmarshal(w.Body.Bytes(), &books)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, books)
+}
+
+func TestUpdateCompanyHandler(t *testing.T) {
+	r := SetUpRouter()
+	r.PUT("/book/:id", UpdateBookHandler)
+	book := Book{
+		ID:     `2`,
+		Name:   "Call Me By Your Name",
+		Author: "Andre Aciman",
+		Price:  "19.99",
+	}
+	jsonValue, _ := json.Marshal(book)
+	reqFound, _ := http.NewRequest("PUT", "/book/"+book.ID, bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, reqFound)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	reqNotFound, _ := http.NewRequest("PUT", "/book/12", bytes.NewBuffer(jsonValue))
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, reqNotFound)
+	assert.Equal(t, http.StatusNotFound, w.Code)
 }
